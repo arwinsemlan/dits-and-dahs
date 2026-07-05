c08a7ce83a9c06d9884f647f7ed31859b4e29e41
arwin <arw.kot10@gmail.com>
2026-07-05 23:08:17 +0200

    I made this project. Initialized it and started with the words.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { onMount } from 'svelte';
+	import './layout.css';
+
+	const { children } = $props();
+
+	let word = $state('');
+
+	onMount(async () => {
+		const commonWords = await fetch('words.txt');
+		let formatedWords = (await commonWords.text()).split('\n');
+		word = formatedWords[Math.floor(Math.random() * formatedWords.length)];
+	});
+</script>
+
+<h1>{word}</h1>
+
+{@render children()}
